d084c6a35674ba63e3225f7b115025c004462e7b
Javed Chauhan <chauhanjaved@gmail.com>
2024-03-19 18:28:39 +0530

    Update: testing different buttons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
-import "../styles/globals.css";
-import { cn } from "../lib/utils"
+import "@/styles/globals.css";
+import { cn } from "@/lib/utils"
 
 const fontSans = FontSans({
   subsets: ["latin"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Mail } from "lucide-react"
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button />
+      <Button>Button</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button>
+        <Mail className="mr-2 h-4 w-4" /> Login with Email
+    </Button>
     </main>
   );
 }
